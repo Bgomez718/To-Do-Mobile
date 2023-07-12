d0d9f95b185a9c72ddb42909c6b5a98a948e2d43
Bryan Gomez <52589312+Bgomez718@users.noreply.github.com>
2023-07-12 20:20:27 +0000

    Update AddTaskPage.jsx and styles.css

--- src/components/AddTaskPage.jsx
+++ src/components/AddTaskPage.jsx
@@ -1,17 +1,17 @@
 import react from "react";
 import { useState } from "react";
 
-export default function AddTaskPage(){
-    const [inputValue,setInputValue] = useState("");
-    function handleInputChange(event){
-        setInputValue(event.target.value);
-    }
-    return(
-    <div>
-      <h1>AHAHAHA</h1>  
+export default function AddTaskPage() {
+  const [inputValue, setInputValue] = useState("");
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+  return (
+    <div className="addTask-container">
+      <h1>AHAHAHA</h1>
       <form>
-         <input type="text" value={inputValue} onChange={handleInputChange}/>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
       </form>
     </div>
-    )
+  );
 }
